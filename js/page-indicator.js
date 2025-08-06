14415ea1d5610f26bf0622f53e5359541903c9ab
janelle <212133343+ellenajj@users.noreply.github.com>
2025-08-06 17:14:37 +0800

    Implemented modal view and added some staff information

--- js/page-indicator.js
+++ js/page-indicator.js
@@ -3,23 +3,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const path = window.location.pathname;
     const page = path.split("/").pop();
 
-    // Check if we're on the homepage (home.html)
-    if (page === "../html/index.html") {
+    if (page === "index.html" || page === "") {
         document.body.classList.add('Home');
+    } else if (page === "about-gates.html") {
+        document.body.classList.add('About');
+    } else if (page === "mission-vision.html") {
+        document.body.classList.add('Mission-Vision');
+    } else if (page === "contact-us.html" || page === "Contact-Us.html") {
+        document.body.classList.add('Contact-Us');
+    } else if (page === "ai.html") {
+        document.body.classList.add('AI-Team');
+    } else if (page === "lakehouse.html") {
+        document.body.classList.add('Lakehouse');
+    } else if (page === "platform.html") {
+        document.body.classList.add('Platform');
+    } else if (page === "interns.html") {
+        document.body.classList.add('Interns');
     }
-}
-if (page === "about.html") {
-    document.body.classList.add('About');
-}
-
-if (page === "mission-vision.html") {
-    document.body.classList.add('Mission-Vision');
-
-}
-if (page === "Contact-Us.html") {
-    document.body.classList.add('Contact-Us');
-
-}
-if (page === "ai.html") {
-    document.body.classList.add('about');
-}
+});
